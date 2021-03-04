eb538753c5238b69b93d0275edd11d54232831ac
ILokalin <51790923+ILokalin@users.noreply.github.com>
2021-03-05 01:01:19 +0500

    feat: update table as fixed thead and scroll

--- src/components/ListPage.jsx
+++ src/components/ListPage.jsx
@@ -1,21 +1,53 @@
 import { TableRow } from "components/TableRow";
 
+const style = {
+  listContainer: {
+    maxHeight: "40vh",
+    overflowY: "auto",
+  },
+  th: {
+    position: "sticky",
+    top: 0,
+    zIndex: 2,
+    transform: "translateY(-1px)",
+  },
+};
+
 export const ListPage = ({ data }) => {
   return (
-    <table className="col-12 mt-5 border-top table table-hover">
-      <caption valign="top">Список регистрационных карточек</caption>
-      <thead>
-        <tr>
-          <th className="text-center col-2 border">id</th>
-          <th className="text-center border">Имя</th>
-          <th className="text-center border">Фамилия</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((card) => (
-          <TableRow {...card} key={card.id} />
-        ))}
-      </tbody>
-    </table>
+    <div className="mt-4">
+      <h2>Список регистрационных карточек</h2>
+      <div className="border-top border-bottom" style={style.listContainer}>
+        <table className="col-12 m-0 table table-hover">
+          <thead>
+            <tr>
+              <th
+                className="text-center bg-light p-2 col-2 border"
+                style={style.th}
+              >
+                id
+              </th>
+              <th
+                className="text-center bg-light p-2 col-5 border"
+                style={style.th}
+              >
+                Имя
+              </th>
+              <th
+                className="text-center bg-light p-2 col-5 border"
+                style={style.th}
+              >
+                Фамилия
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((card) => (
+              <TableRow {...card} key={card.id} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
