6349b561c639570e2c8b6546edb13f0ae2fff03e
ILokalin <51790923+ILokalin@users.noreply.github.com>
2021-03-06 03:44:39 +0500

    feat: add scrollBy for active table row

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,21 +1,58 @@
+import { useRef, useEffect, useLayoutEffect } from "react";
 import { setActive } from "./../redux";
 import { useDispatch } from "react-redux";
 
-export const TableRow = ({ id, firstName, lastName, isActive }) => {
+const style = {
+  row: {
+    cursor: "pointer",
+  },
+  rowClass: "bg-primary text-light",
+};
+
+export const TableRow = ({
+  id,
+  firstName,
+  lastName,
+  isActive,
+  ...otherProps
+}) => {
   const dispatch = useDispatch();
-  const rowClass = isActive ? "bg-primary text-light" : "border";
-  const style = {
-    row: {
-      cursor: "pointer",
-    },
-  };
+  const row = useRef(null);
 
   const rowSelectHandler = () => {
     dispatch(setActive(id));
   };
 
+  useEffect(() => {
+    if (isActive) {
+      const { container, thead } = otherProps;
+      const containerTop = container.current.getBoundingClientRect().top;
+      const containerHeight = container.current.getBoundingClientRect().height;
+      const { top, bottom } = row.current.getBoundingClientRect();
+      const topOffset = thead.current.getBoundingClientRect().height;
+
+      if (top < containerTop + topOffset) {
+        container.current.scrollBy({
+          top: top - containerTop - topOffset,
+          behavior: "smooth",
+        });
+      } else if (bottom > containerTop + containerHeight) {
+        container.current.scrollBy({
+          top: bottom - containerTop - containerHeight,
+          behavior: "smooth",
+        });
+      }
+    }
+    // eslint-disable-next-line
+  }, [isActive]);
+
   return (
-    <tr className={rowClass} style={style.row} onClick={rowSelectHandler}>
+    <tr
+      className={`${isActive ? style.rowClass : ""}`}
+      style={style.row}
+      onClick={rowSelectHandler}
+      ref={row}
+    >
       <td className="p-2 border">{id}</td>
       <td className="p-2 border">{firstName}</td>
       <td className="p-2 border">{lastName}</td>
